73228b82ff35361bf5e06f4f1abd7146f5e8e399
Shuflduf <shuflduf@gmail.com>
2024-08-11 12:59:42 -0600

    actual blue

---
+++ scripts/ghosts/blue.gd
@@ -0,0 +1,18 @@
+extends Ghost
+
+var red: Ghost
+var pink: Ghost
+
+func _ready() -> void:
+	super()
+	pink = get_parent().find_child("Pink")
+	red = get_parent().find_child("Red")
+
+
+func set_target() -> Vector2:
+	var dir = pink.set_target() - red.global_position
+	dir *= 2
+	print(dir)
+
+
+	return dir
--- scripts/ghosts/pink.gd
+++ scripts/ghosts/pink.gd
@@ -2,6 +2,6 @@ extends Ghost
 
 
 
-func set_target():
+func set_target() -> Vector2:
 	var target = player.global_position - (player.last_dir * 200)
 	return target
